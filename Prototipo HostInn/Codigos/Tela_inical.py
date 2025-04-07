4f83d45b4eab722d271fe575dd3677d2c1f1252d
EndrewFerreira <ferreira10980@gmail.com>
2025-04-07 16:58:26 -0300

    Merge branch 'main' of https://github.com/EndrewFerreira/HOSTIIN

--- Prototipo HostInn/Codigos/Tela_inical.py
+++ Prototipo HostInn/Codigos/Tela_inical.py
@@ -7,13 +7,13 @@ import Menu
 
 
 
-# Conexão com o banco de dados
 banco = pymysql.connect(
     host="localhost",
     user="root",
-    passwd="",
+    passwd="#mortadela1507",
     database="bd_teste2"
 )
+
 
 def verificar_permissao():
     cursor = banco.cursor()
@@ -129,7 +129,7 @@ icon_eye_open = QIcon("Icones/visibility.png")
 icon_eye_closed = QIcon("Icones/visibility_off.png")
 
 #===========================( Login/ Cadastro )=============================================
-login_register = uic.loadUi(r"C:\Users\11054836\Desktop\PI\HOSTIIN\Prototipo HostInn\Telas\TELA_LOGIN_CADASTRO_2.ui")
+login_register = uic.loadUi(r"C:\Users\ferre\Desktop\PROJETO HOSTINN\HOSTIIN\Prototipo HostInn\Telas\TELA_LOGIN_CADASTRO_2.ui")
 login_register.setWindowTitle("HostInn")
 login_register.stackedWidget.setCurrentIndex(0)
 login_register.lineEdit_passwrd.setEchoMode(QLineEdit.EchoMode.Password)
